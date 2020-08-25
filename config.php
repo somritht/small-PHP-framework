7454e4c51cde0ca5bf97d3d070159c20a439fe1e
somrith <kuysomrith@gmail.com>
2020-08-25 22:30:46 +0700

    ready for dev

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ return [
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
         'options' => [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT
+            // PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT
         ]
     ]
 
