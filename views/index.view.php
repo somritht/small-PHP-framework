03c9c7ff53d99c7a7e895a8f336c9ae4a4e85d10
somrith <kuysomrith@gmail.com>
2020-08-25 22:29:51 +0700

    fix db error

--- views/index.view.php
+++ views/index.view.php
@@ -2,5 +2,11 @@
 <?php require('layouts/header.php'); ?>
 
 <h2> Index.view.php</h2>
+<?php
+
+foreach ($tasks as $key => $value) {
+    echo $value->first_name;
+}
+?>
 
 <?php require('layouts/footer.php'); ?>
